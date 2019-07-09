8ca9e365b8df0f223999e3894b2683a647d76353
Mr-Trung <letran3691@gmail.com>
2019-07-09 21:12:40 +0700

    'edit_python'

--- installpython3.6.sh
+++ installpython3.6.sh
@@ -5,35 +5,7 @@ sleep 3
 systemctl stop firewalld
 systemctl disable firewalld
 
-
-## installation required GCC
-echo 'installation required GCC'
-sleep 4
-yum install gcc openssl-devel bzip2-devel wget -y
-
-## Download Python
-
-echo 'Download Python'
-
-sleep 4
-cd /usr/src
-wget https://www.python.org/ftp/python/3.6.8/Python-3.6.8.tgz
-
-## Extract
-tar xzf Python-3.6.8.tgz
-
-# make and install
-
-echo 'make and install'
-
-sleep 4
-cd Python-3.6.8
-./configure --enable-optimizations
-make altinstall
-
-rm -f /usr/src/Python-3.6.8.tgz
-
-echo 'python3.6 -V'
+yum install -y epel-release; yum install -y python36 python36-devel python36-setuptools
 
 chmod -R +x /root/domain/*.py
 
